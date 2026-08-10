efacb9b2ddee32cd5d03609e69d1e155f289d0c1
cintiaomaas <cintiaomaas@gmail.com>
2026-08-09 21:16:20 -0300

    criando cenários de testes de cadastros de usuários

--- playwright.config.js
+++ playwright.config.js
@@ -1,13 +1,6 @@
 // @ts-check
 import { defineConfig, devices } from '@playwright/test';
-
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
+import 'dotenv/config';
 
 /**
  * @see https://playwright.dev/docs/test-configuration
@@ -15,15 +8,15 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
   testDir: './tests/e2e',
   /* Run tests in files in parallel */
-  fullyParallel: true,
+  fullyParallel: false, //true = executa os testes em paralelo, false = executa os testes de forma sequencial
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 1 : 1, //1 = executa os testes de forma sequencial, 2 ou mais = executa os testes em paralelo
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [['html', { open: 'never' }]], //informa que o relatório do teste será gerado em html e não será aberto automaticamente
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
@@ -31,26 +24,27 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    headless: false, //true = faz o teste sem abrir o navegador, false = abre o navegador e faz o teste
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
   },
 
-  /* Configure projects for major browsers */
+   /* Configure projects for major browsers */
   projects: [
     {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      name: 'chrome',
+      use: { ...devices['Desktop Chrome'], channel: 'chrome' },
     },
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
 
     /* Test against mobile viewports. */
     // {
@@ -80,4 +74,3 @@ export default defineConfig({
   //   reuseExistingServer: !process.env.CI,
   // },
 });
-
---
+++ tests/e2e/cadastrarUsuarios.spec.js
@@ -0,0 +1,49 @@
+import { test, expect } from '@playwright/test';
+import { deleteUserByEmail } from '../support/database';
+import { criarConta } from '../support/pages/criarConta';
+
+const testUser = {
+  name: 'João da Silva',
+  email: 'joao.silva@example.com',
+  password: 'senha123',
+};
+
+test.describe('Cadastro de Usuários', () => {
+  test.beforeEach(async () => {
+    await deleteUserByEmail(testUser.email);
+  });
+
+  test.afterEach(async () => {
+    await deleteUserByEmail(testUser.email);
+  });
+
+  test('Deve cadastrar um novo usuário com sucesso', async ({ page }) => {
+    await criarConta(page, testUser.name, testUser.email, testUser.password);
+    await page.getByRole('button', { name: 'Criar conta e entrar' }).click();
+    await expect(
+      page.getByRole('heading', { name: 'Olá, João' }),
+    ).toBeVisible({ timeout: 15_000 });
+  })
+
+  test('Não deve permitir e-mail inválido', async ({ page }) => {
+    await criarConta(page, testUser.name, 'joao@emailinvalido', testUser.password);
+    await page.getByRole('button', { name: 'Criar conta e entrar' }).click();
+    await expect(page.getByText('Confira os dados informados para criar sua conta.')).toBeVisible();
+  })
+
+  test('Não deve permitir senha com menos de 8 caracteres', async ({ page }) => {
+    await criarConta(page, testUser.name, testUser.email, '123');
+    await page.getByRole('button', { name: 'Criar conta e entrar' }).click();
+    expect(
+      await page
+        .getByRole('textbox', { name: /Senha/ })
+        .evaluate((input) => input.validationMessage),
+    ).toContain('Aumente este texto para 8 caracteres ou mais');
+  })
+
+  test('Não deve permitir cadastro com e-mail já existente', async ({ page }) => {
+    await criarConta(page, 'teste', 'teste@teste.com', 'teste1234');
+    await page.getByRole('button', { name: 'Criar conta e entrar' }).click();
+    await expect(page.getByRole('alert').filter({ hasText: 'Já existe uma conta cadastrada com este e-mail.' })).toBeVisible();
+  })
+})
